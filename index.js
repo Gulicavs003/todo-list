47154a55e150a38c39d7a6c7e915f3e4c9b4af25
Giuliano <giulianodamico2019@gmail.com>
2021-03-03 02:21:59 -0300

    Subida y recopilación de datos

---
+++ index.js
@@ -0,0 +1,34 @@
+const db = firebase.firestore();
+
+
+const taskForm = document.getElementById('task-form');
+
+const saveTask = (title, description)=>
+ db.collection('tasks').doc().set({
+    title,
+    description
+})
+
+const getTasks= () => db.collection('tasks').get();
+
+window.addEventListener('DOMContentLoaded', async (e) =>{
+ const querySnapshot = await getTasks();
+    querySnapshot.forEach(doc => {
+    console.log(doc.data())
+})
+ })
+
+
+taskForm.addEventListener('submit', async (e)=> {
+    e.preventDefault();
+
+    const title = taskForm['task-title'];
+    const description = taskForm['task-description'];
+
+    await saveTask(title.value, description.value);
+
+    taskForm.reset();
+    title.focus();
+
+    
+})
